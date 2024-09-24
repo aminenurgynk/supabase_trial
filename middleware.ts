4efb9d9cad01de080bd3ae18dae6a23bd264d31f
aminenurgynk <amine.gynk@gmail.com>
2024-09-24 12:11:55 +0300

    I like the using source control graph

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 // middleware.ts
 
 import { type NextRequest } from "next/server";
-import { updateSession } from "./utils/supabase/middleware";
+import { updateSession } from "./app/utils/supabase/middleware";
   
 export async function middleware(request: NextRequest) {
   return await updateSession(request);
