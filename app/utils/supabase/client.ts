3281a6f81c32e78f1c3a5354c5d41c166221ee19
aminenurgynk <amine.gynk@gmail.com>
2024-09-24 12:08:20 +0300

    supabase configuration

---
+++ app/utils/supabase/client.ts
@@ -0,0 +1,10 @@
+// utils/supabase/client.ts
+
+import { createBrowserClient } from "@supabase/ssr";
+
+export function createClient() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  );
+}
