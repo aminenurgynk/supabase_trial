f07397ac8d5728dfb548881073f7ea39ad1a519d
aminenurgynk <amine.gynk@gmail.com>
2024-09-25 10:26:15 +0300

    it was some important mistake....

--- actions/auth/actions.ts
+++ actions/auth/actions.ts
@@ -2,7 +2,7 @@
 
 "use server";
 
-import { createClient } from "@/utils/supabase/client";
+import { createClient } from "@/utils/supabase/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
